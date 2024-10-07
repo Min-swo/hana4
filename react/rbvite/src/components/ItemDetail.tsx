0deeb1445fd0686b54a64c10b631325cdd2975e1
Min-swo <msms0226@g.skku.edu>
2024-10-07 11:07:00 +0900

    Link

--- react/rbvite/src/components/ItemDetail.tsx
+++ react/rbvite/src/components/ItemDetail.tsx
@@ -1,5 +1,5 @@
-import { useOutletContext, useParams } from 'react-router-dom';
-import { CartItem, useSession } from '../hooks/session-context';
+import { useParams } from 'react-router-dom';
+import { useSession } from '../hooks/session-context';
 import clsx from 'clsx';
 
 export default function ItemDetail() {
@@ -7,8 +7,8 @@ export default function ItemDetail() {
     session: { cart },
   } = useSession();
   const { id: itemId } = useParams();
-  const item = useOutletContext<CartItem>();
-  // const item = cart.find(({ id }) => id === Number(itemId));
+  // const item = useOutletContext<CartItem>();
+  const item = cart.find(({ id }) => id === Number(itemId));
   console.log(item);
   return (
     <>
